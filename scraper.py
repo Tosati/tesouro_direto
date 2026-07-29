4530d0511fa4ea12383927dce7b0b6b453125509
Tosati <tosati@gmail.com>
2026-07-29 18:35:48 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,17 @@
 import requests
 import json
 
-# Códigos SGS do Banco Central para títulos do Tesouro Direto
+# Códigos SGS do Banco Central para Tesouro Selic e Tesouro IPCA+
 TITULOS = {
     "Tesouro Selic 2027": 4390,
     "Tesouro Selic 2029": 4391,
-    "Tesouro IPCA+ 2035": 4393,
-    "Tesouro IPCA+ 2045": 4394
+    "Tesouro Selic 2031": 4392,
+    "Tesouro IPCA+ 2029": 4393,
+    "Tesouro IPCA+ 2032": 4394,
+    "Tesouro IPCA+ 2035": 4395,
+    "Tesouro IPCA+ 2040": 4396,
+    "Tesouro IPCA+ 2045": 4397,
+    "Tesouro IPCA+ 2055": 4398
 }
 
 def obter_serie(codigo):
